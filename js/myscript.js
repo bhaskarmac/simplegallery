0ce16999af971d1de3d8ae9c84195b7f8a46cd30
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-08-01 23:54:21 +0530

    Adding feature of showing and hiding overlay when clicked on image

--- js/myscript.js
+++ js/myscript.js
@@ -12,25 +12,51 @@ jQuery(document).ready(function() {
 	}); //getJSON ends here
 
 	/**
+	 * [showOverlay : method to show overlay]
+	 */
+	 function showOverlay() {
+	 	jQuery('.overlay').css('width', '100%');;
+	 }
+
+	/**
+	 * [hideOverlay : method to hide overlay]
+	 */
+	 function hideOverlay() {
+	 	jQuery('.overlay').css('width', '0%');;
+	 }
+
+	/**
 	 * [displayImages : method to append the images in body and display]
 	 * @param  {[JSON Array]} imageJSON [accepts the JSON array of images to display]
 	 */
-	function displayImages(imageJSON) {
-		console.log('In displayImages=>', imageJSON);
-		var singleImage;
-		for (var i = 0;i<imageJSON.length; i++) {
-			singleImage = '<div class="gallery">'+
-			'<a href="'+imageJSON[i].imgSource+'">'+
-			'<img src="'+imageJSON[i].imgSource+'" alt="'+imageJSON[i].name+'" width="300" height="200">'+
-			'</a>'+
-			'<div class="desc">'+imageJSON[i].name+'</div>'+
-			'</div>';
+	 function displayImages(imageJSON) {
+	 	console.log('In displayImages=>', imageJSON);
+	 	var singleImage;
+	 	for (var i = 0; i<imageJSON.length; i++) {
+	 		singleImage = '<div class="gallery">'+
+	 		'<a href="'+imageJSON[i].imgSource+'" class="imgLink">'+
+	 		'<img src="'+imageJSON[i].imgSource+'" alt="'+imageJSON[i].name+'" width="300" height="200">'+
+	 		'</a>'+
+	 		'<div class="desc">'+imageJSON[i].name+'</div>'+
+	 		'</div>';
 
-			jQuery('.galleryContainer').append(singleImage);
+	 		jQuery('.galleryContainer').append(singleImage);
 		} // for loop ends here
-		
-
 	} //displayImages ends here
+
+	//Event handler for image link when clicked 
+	jQuery(document).on('click','.imgLink',function(e){
+		e.preventDefault();
+		console.log('imgLink clicked');
+		showOverlay();
+		return false;
+	});
+
+	//Event handler for close button on overlay to close it
+	jQuery('.closebtn').click(function(event) {
+		hideOverlay();
+	});
+
 
 
 }); //document ready ends here
