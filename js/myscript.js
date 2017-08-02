6501b4c373752d30d5171518eb58273f0eaa645b
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-08-02 23:08:15 +0530

    Adding navigation features to gallery on overlay
    Adding fade effects for the overlay
    
    Adding bootstrap file
    
    Adding more images
    
    Improved markup for the images
    
    Removing unwanted styles

--- js/myscript.js
+++ js/myscript.js
@@ -16,7 +16,6 @@ jQuery(document).ready(function() {
 	 */
 	 function showOverlay() {
 	 	jQuery('.overlay').fadeIn('slow');
-	 	// jQuery('.overlay').css('width', '100%');;
 	 }
 
 	/**
@@ -24,7 +23,6 @@ jQuery(document).ready(function() {
 	 */
 	 function hideOverlay() {
 	 	jQuery('.overlay').fadeOut('slow');
-	 	// jQuery('.overlay').css('width', '0%');;
 	 }
 
 	/**
@@ -35,12 +33,9 @@ jQuery(document).ready(function() {
 	 	console.log('In displayImages=>', imageJSON);
 	 	var singleImage;
 	 	for (var i = 0; i<imageJSON.length; i++) {
-	 		singleImage = '<div class="gallery">'+
-	 		'<a href="'+imageJSON[i].imgSource+'" class="imgLink" data-slide-index="'+ (i+1) +'">'+
-	 		'<img src="'+imageJSON[i].imgSource+'" alt="'+imageJSON[i].name+'" width="300" height="200">'+
-	 		'</a>'+
-	 		'</div>';
-
+	 		singleImage = '<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4">'+
+	 		'<img class="img-responsive imgLink" alt="'+imageJSON[i].name+'" src="'+imageJSON[i].imgSource+'">'+
+	 		'</li>';
 	 		jQuery('.galleryContainer').append(singleImage);
 		} // for loop ends here
 	} //displayImages ends here
@@ -51,11 +46,21 @@ jQuery(document).ready(function() {
 		console.log('imgLink clicked');
 		showOverlay();
 
-		jQuery('.bigImage').attr('src', 'loading.png');
-		var imgSrc = jQuery(this).attr('href');
+		//getting image data
+		var imgSrc = jQuery(this).attr('src');
+		var imgNumber = jQuery(this).parent('li').index();  
+
+		//assigning the next and previous image numbers to navigate
+		jQuery('.next').attr('href', imgNumber + 2);
+		jQuery('.prev').attr('href', imgNumber);
+
+		// jQuery('.bigImage').attr('src', 'loading.png');
+
 		jQuery('.bigImage').attr('src', imgSrc).load(function() {
-			jQuery(this).fadeIn('slow')
+			jQuery(this).fadeIn('slow');
 		});
+
+		jQuery('a.navigationControls').trigger('click');
 		return false;
 	});
 
@@ -70,17 +75,56 @@ jQuery(document).ready(function() {
 		}
 	});
 
-	jQuery(document).on('click','.prev',function(e){
-		e.preventDefault();
-		console.log('prev clicked');
+	jQuery(document).on('click','.overlay',function(e){
+		hideOverlay();
 		return false;
 	});
 
-	jQuery(document).on('click','.next',function(e){
+
+	jQuery(document).on('click','.navigationControls',function(e){
 		e.preventDefault();
-		console.log('next clicked');
+
+		//retrieving the image data to process navigation details
+		var imgNumber = jQuery(this).attr('href');
+		var imgSrc = jQuery('ul.row li:nth-child('+ imgNumber +') img').attr('src');  
+		var imgName = jQuery('ul.row li:nth-child('+ imgNumber +') img').attr('alt');  
+
+		//Assigning the image in larger view           
+		jQuery('.bigImage').attr('src', imgSrc);
+		jQuery('.imageTitle').html(imgName);
+
+		//Calculating the next and previous position
+		var newPrevNo = parseInt(imgNumber) - 1; 
+		var newNextNo = parseInt(newPrevNo) + 2; 
+
+		//Assigning new positions
+		if(jQuery(this).hasClass('prev')){               
+			jQuery(this).attr('href', newPrevNo); 
+			jQuery('a.next').attr('href', newNextNo);
+		}else{
+			jQuery(this).attr('href', newNextNo); 
+			jQuery('a.prev').attr('href', newPrevNo);
+		}
+
+		var totalImages = jQuery('ul.row li').length + 1; 
+
+		//Checking the total number of image to show/hide the navigation controls
+		if(totalImages === newNextNo){
+			jQuery('a.next').hide();
+		}else{
+			jQuery('a.next').show()
+		}    
+
+		if(newPrevNo === 0){
+			jQuery('a.prev').hide();
+		}else{
+			jQuery('a.prev').show()
+		}
+
 		return false;
 	});
+
+
 
 
 
